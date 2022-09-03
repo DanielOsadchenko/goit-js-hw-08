da8919e9079dd3aa4df96022cbf9f771afa5f416
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-09-03 18:04:17 +0200

    task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import Simplelightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map((image) => 
+`<li><a class="gallery__item" href=${image.original}>
+  <img
+      class="gallery__image"
+      src=${image.preview}
+      alt=${image.description}
+    />
+  </a></li>`).join("");
+
+gallery.innerHTML = markup;
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionDelay: 250,});
