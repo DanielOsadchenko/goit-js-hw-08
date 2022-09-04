b831b93dc284fffec3a86a2c53cf05d094e7459e
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-09-04 14:30:42 +0200

    completed task 2, completed half of task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+var throttle = require('lodash.throttle');
+
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input');
+const messageRef = document.querySelector('textarea');
+
+const formData = {
+  email: '',
+  message: '',
+}
+formRef.addEventListener('submit', function (event) {
+  event.preventDefault();
+  console.log({email: emailRef.value, message: messageRef.value});
+  event.currentTarget.reset();
+  localStorage.clear();
+})
+formRef.addEventListener('input', throttle(onForm, 500));
+
+function onForm(event) {
+  const emailValue = event.currentTarget.elements.email.value;
+  const messageValue = event.currentTarget.elements.message.value;
+
+    formData.email = emailValue;
+    formData.message = messageValue;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  
+}
+function getCurrentValuesOnForm() {
+  const localStorageData = localStorage.getItem('feedback-form-state');
+  if (localStorageData) {
+    const currentData = JSON.parse(localStorageData);
+    emailRef.value = currentData.email;
+    messageRef.value = currentData.message;
+}
+};
+getCurrentValuesOnForm();
+
+
+
 
